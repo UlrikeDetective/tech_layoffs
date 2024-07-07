3e8f08d705a460bcb2a5ed416a2d59100192cf6f
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-07-07 21:26:15 +0200

    Update tech_layoffs_location_map.py

--- tech_layoffs_analysis/tech_layoffs_location_map.py
+++ tech_layoffs_analysis/tech_layoffs_location_map.py
@@ -1,4 +1,4 @@
-df.to_csv('/tech_layoffs_csv/layoffs_location_with_coordinates.csv', index=False)
+df.to_csv('/layoffs_location_with_coordinates.csv', index=False)
 
 display(df.head())
 display(df.info(verbose=True))
