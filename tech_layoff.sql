fd47cfcd7bbdb22513554d588e6fbb5eca97ebbb
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-07-14 18:05:06 +0200

    Create tech_layoff.sql

---
+++ tech_layoff.sql
@@ -0,0 +1,31 @@
+Create database Tech_layoffs;
+Use Tech_layoffs;
+
+
+SET GLOBAL local_infile = 'ON';
+SHOW VARIABLES LIKE 'local_infile';
+SHOW VARIABLES LIKE "secure_file_priv";
+
+-- drop table forbes_names; 
+
+CREATE TABLE Q2_2024 (
+    # INT UNIQUE,
+    Company VARCHAR(100),
+    Location_HQ VARCHAR(100),
+    Region VARCHAR(50),
+    State VARCHAR(50),
+    Country VARCHAR(100),
+    Continent VARCHAR(100),
+    Laid_Off FLOAT,
+    Date_layoffs DATE,
+    Percentage FLOAT,
+    Company_Size_before_Layoffs INT,
+    Company_Size_after_layoffs INT,
+    Industry VARCHAR(50),
+    Stage VARCHAR(50),
+    Money_Raised_in__mil INT,
+    Year INT,
+    latitude FLOAT,
+    longitude FLOAT,
+    PRIMARY KEY (#),
+);
